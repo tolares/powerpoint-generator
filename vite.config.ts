413d0276f6894fedba523447ac961e1ec29dcbfc
h.lagarde <h.lagarde@criteo.com>
2024-04-15 09:31:04 +0200

    Fix Logo import in App

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,6 @@ export default defineConfig({
     sourcemap: true,
   },
   define: {
-    "process.env": {},
+    'process.env': {},
   },
 });
